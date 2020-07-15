1409d82cb487eb61d754538ce22798203b189ea5
ziya <ziya.yekta@gmail.com>
2020-07-15 00:55:25 -0700

    1.12 answers added

--- part1/unicafe/src/index.js
+++ part1/unicafe/src/index.js
@@ -39,11 +39,23 @@ const Statistics = props => {
 
 }
 
+
+
 const App = () => {
 	const [good, setGood] = useState(0);
 	const [neutral, setNeutral] = useState(0);
 	const [bad, setBad] = useState(0);
 	const [history, setHistory] = useState([]);
+	const [selected, setSelected] = useState(0);
+
+	const anecdotes = [
+  		'If it hurts, do it more often',
+  		'Adding manpower to a late software project makes it later!',
+  		'The first 90 percent of the code accounts for the first 90 percent of the development time...The remaining 10 percent of the code accounts for the other 90 percent of the development time.',
+  		'Any fool can write code that a computer can understand. Good programmers write code that humans can understand.',
+  		'Premature optimization is the root of all evil.',
+  		'Debugging is twice as hard as writing the code in the first place. Therefore, if you write the code as cleverly as possible, you are, by definition, not smart enough to debug it.'
+]
 
 	const goodHandler = () => {
 		setHistory(history.concat(1));
@@ -69,6 +81,12 @@ const App = () => {
 
 	const positiveScore = () => ((good / history.length) * 100) + '%';
 
+	const getRandomNumber = () => Math.floor(Math.random() * Math.floor(anecdotes.length));
+
+	const anecdoteHandler = () => {
+		setSelected(getRandomNumber());
+	}
+
 	if (history.length === 0) {
 		return (
 			<div>
@@ -78,6 +96,8 @@ const App = () => {
 				<Button handler={badHandler} text='bad' />
 				<Heading text='Statistics' />
 				<p>No feedback given</p>
+				<p>{anecdotes[selected]}</p>
+				<Button handler={anecdoteHandler} text='next anecdote' />
 			</div>
 			)
 	}
@@ -88,9 +108,9 @@ const App = () => {
 			<Button handler={neutralHandler} text='neutral' />
 			<Button handler={badHandler} text='bad' />
 			<Heading text='Statistics' />
-		
 			<Statistics good={good} neutral={neutral} bad={bad} history={history} avg={avg} positiveScore={positiveScore} />
-
+			<p>{anecdotes[selected]}</p>
+			<Button handler={anecdoteHandler} text='next anecdote' />
 		</div>
 		)
 }

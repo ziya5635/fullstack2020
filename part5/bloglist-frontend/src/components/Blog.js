749d8968cedb38a5453b8cd06e01aa7e8f62f438
ziya <ziya.yekta@gmail.com>
2020-08-24 08:18:44 -0700

    5.6 & 5.7 answers added

--- part5/bloglist-frontend/src/components/Blog.js
+++ part5/bloglist-frontend/src/components/Blog.js
@@ -1,8 +1,28 @@
-import React from 'react'
-const Blog = ({ blog }) => (
-  <div>
-    {blog.title} {blog.author}
-  </div>
-)
+import React, {useState} from 'react'
+
+const Blog = ({ blog }) => {
+	const [visible, setVisible] = useState(false)
+
+	const handler = event => setVisible(!visible)
+
+	if (!visible) {
+		return(
+		  <div className='blog'>
+    		{blog.title} {blog.author} <button onClick={handler}>show</button>
+ 		 </div>
+ 		 )
+	} else {
+		return(
+		<div className='blog'>
+			<div>{blog.title} <button onClick={handler}>hide</button></div>
+			<div>{blog.url}</div>
+			<div>likes {blog.likes} <button>like</button></div>
+			<div>{blog.author}</div>
+		</div>
+		)
+	}
+
+
+}
 
 export default Blog
--- part5/bloglist-frontend/src/components/RenderBlogs.js
+++ part5/bloglist-frontend/src/components/RenderBlogs.js
@@ -4,7 +4,7 @@ import Blog from './Blog'
 const RenderBlogs = ({user, blogs}) => {
 	if(user){
        return blogs.map(blog =>
-        <Blog key={blog.id} blog={blog} />
+        <Blog key={blog.id} blog={blog} /> 
       )
 	} return null
 }

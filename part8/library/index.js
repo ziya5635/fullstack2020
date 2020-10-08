aa161a39d81e2f8cfa9859ec14d55e971a777f3a
reza <ziya.yekta@gmail.com>
2020-10-08 04:30:43 -0700

    8.4 answers added

--- part8/library/index.js
+++ part8/library/index.js
@@ -101,7 +101,7 @@ const typeDefs = gql`
   type Query {
     bookCount: Int!
     authorCount: Int!
-    allBooks: [Book!]!
+    allBooks(author: String): [Book!]!
     allAuthors: [Author]!
   }
 `
@@ -110,7 +110,12 @@ const resolvers = {
   Query: {
     bookCount: () => books.length,
     authorCount: () => authors.length,
-    allBooks: () => books,
+    allBooks: (root, args) => {
+      if (args.author) {
+        return books.filter(book => book.author === args.author)
+      }
+      return books
+    },
     allAuthors: () => {
      const res = authors.map(author => {
         return books.filter(book => book.author === author.name)

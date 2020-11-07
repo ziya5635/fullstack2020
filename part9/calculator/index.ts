6d5ded41e1a3d0e67f53088ec7438b37b2e15d48
reza <ziya.yekta@gmail.com>
2020-11-07 01:13:11 -0800

    9.5 answers added

--- part9/calculator/index.ts
+++ part9/calculator/index.ts
@@ -1,16 +1,27 @@
 
 import express from 'express';
+import { calculateBmi } from './src/bmiCalculator'
+
 require('dotenv').config({ path: __dirname+'/.env' });
 
 const app = express();
-
 
 app.get('/hello', (_req, res) => {
 	res.send('Hello Full Stack');
 })
 
+app.get('/bmi', (req, res) => {
+	const height:number = Number(req.query.height);
+	const weight:number = Number(req.query.weight);
+	if(height && weight){
+		res.json(calculateBmi(height, weight));
+	}else{
+		res.send({error: "malformatted parameters"});
+	}
+})
+
 const port = process.env.PORT;
 
 app.listen(port, () => {
-	console.log(`server is running on port ${port}`)
+	console.log(`server is running on port ${port}`);
 })
--- part9/calculator/src/bmiCalculator.ts
+++ part9/calculator/src/bmiCalculator.ts
@@ -1,19 +1,29 @@
 
-const calculateBmi = (height: number, weight: number) : void => {
+interface Bmi {
+	height:number,
+	weight:number,
+	bmi:string,
+}
+
+export const calculateBmi = (height: number, weight: number) : Bmi => {
 	const heightToMeter:number = height / 100;
 	const res:number = weight / (heightToMeter**2);
 	if (res < 18.5) {
 		console.log('underwight');
+		return {height: height, weight: weight, bmi:'underwight'};
 	}else if (res >=18.5 && res < 25) {
 		console.log('normal (healthy weight)');
+		return {height: height, weight: weight, bmi:'normal (healthy weight)'};
 	}else if(res >= 25 && res < 30) {
 		console.log('over weight');
+		return {height: height, weight: weight, bmi:'over weight'};
 	}else{
 		console.log('obese');
+		return {height: height, weight: weight, bmi:'obese'};
 	}
 }
 
-
+/*
 try {
 	const height:number = Number(process.argv[2]);
 	const weight:number = Number(process.argv[3]);
@@ -23,4 +33,4 @@ try {
 	calculateBmi(height, weight);
 }catch(error){
 	console.log(error.message)
-}
+}*/
